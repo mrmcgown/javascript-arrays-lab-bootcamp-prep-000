c04d9b8635b5dad10d7503329e448e0dd22e603a
Michael R. McGown <mrmcgown@gmail.com>
2018-03-16 17:16:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(Ralph) {
+  return kittens.push("Ralph");
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyPrependKitten(Bob) {
+  return kittens.unshift("Bob");
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(Broom) {
+  return [...kittens,"Broom"];
+}
+
+function prependKitten(Arnold) {
+  return ["Arnold", ...kittens];
+}
+
+function removeLastKitten() [
+  return ]
